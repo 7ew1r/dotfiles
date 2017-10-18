8082b1e198e1491bd59e31c6565d36ca1ac049c8
TEWi_R <art.sht.0317@gmail.com>
2017-10-19 01:58:11 +0900

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,23 +5,39 @@
 ;; You may delete these explanatory comments.
 
 ;;; packages:
-(require 'package)
+;; (require 'package)
 ;; (require 'cask)
 ;; (require 'cask "~/.cask/cask.el")
 ;; (cask-initialize)
 ;; (package-initialize)
+;;; Code:
+(setq-default gc-cons-percentage 0.5)
 
+(if window-system
+    (tool-bar-mode -1)
+  (menu-bar-mode -1))
 
+;;; theme:
+(load-theme 'wombat)
+
+;;; packages:
 (when (or (require 'cask "~/.cask/cask.el" t)
 	  (require 'cask nil t))
   (cask-initialize))
 (package-initialize)
 
+;;; Variables:
+(setq make-backup-files nil)
+(setq delete-auto-save-files t)
 
-(global-linum-mode)
+;; White space
+(setq-default show-trailing-whitespace t)
+
+(require 'linum)
+(global-linum-mode 1)
 (setq linum-format "%4d ")
 (global-hl-line-mode)
-(load-theme 'wombat)
+
 
 (add-hook 'after-init-hook #'global-flycheck-mode)
 
@@ -30,8 +46,75 @@
 (with-eval-after-load 'flycheck
   (flycheck-pos-tip-mode))
 
+(with-eval-after-load 'flycheck
+  '(add-hook 'flycheck-mode-hook 'flycheck-popup-tip-mode))
+
+
 ;; Key binding
 (define-key global-map (kbd "M-SPC") 'set-mark-command)
+
+;; Auto-Complete
+(require 'auto-complete-config)
+(ac-config-default)
+(add-to-list 'ac-modes 'text-mode)         ;; text-modeでも自動的に有効にする
+(add-to-list 'ac-modes 'fundamental-mode)  ;; fundamental-mode
+(add-to-list 'ac-modes 'org-mode)
+(add-to-list 'ac-modes 'yatex-mode)
+(ac-set-trigger-key "TAB")
+(setq ac-use-menu-map t)       ;; 補完メニュー表示時にC-n/C-pで補完候補選択
+(setq ac-use-fuzzy t)          ;; 曖昧マッチ
+
+;; smooth-scroll
+(require 'smooth-scroll)
+(smooth-scroll-mode t)
+(setq smooth-scroll/vscroll-step-size 4)
+
+;; magit
+(require 'magit)
+
+;; helm
+(require 'helm)
+(require 'helm-config)
+
+;; The default "C-x c" is quite close to "C-x C-c", which quits Emacs.
+;; Changed to "C-c h". Note: We must set "C-c h" globally, because we
+;; cannot change `helm-command-prefix-key' once `helm-config' is loaded.
+(global-set-key (kbd "C-c h") 'helm-command-prefix)
+(global-unset-key (kbd "C-x c"))
+
+(define-key helm-map (kbd "<tab>") 'helm-execute-persistent-action) ; rebind tab to run persistent action
+(define-key helm-map (kbd "C-i") 'helm-execute-persistent-action) ; make TAB work in terminal
+(define-key helm-map (kbd "C-z")  'helm-select-action) ; list actions using C-z
+
+(when (executable-find "curl")
+  (setq helm-google-suggest-use-curl-p t))
+
+(setq helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    8 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t
+      helm-echo-input-in-header-line t)
+
+(defun spacemacs//helm-hide-minibuffer-maybe ()
+  "Hide minibuffer in Helm session if we use the header line as input field."
+  (when (with-helm-buffer helm-echo-input-in-header-line)
+    (let ((ov (make-overlay (point-min) (point-max) nil nil t)))
+      (overlay-put ov 'window (selected-window))
+      (overlay-put ov 'face
+                   (let ((bg-color (face-background 'default nil)))
+                     `(:background ,bg-color :foreground ,bg-color)))
+      (setq-local cursor-type nil))))
+
+
+(add-hook 'helm-minibuffer-set-up-hook
+          'spacemacs//helm-hide-minibuffer-maybe)
+
+(setq helm-autoresize-max-height 0)
+(setq helm-autoresize-min-height 20)
+(helm-autoresize-mode 1)
+
+(helm-mode 1)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.

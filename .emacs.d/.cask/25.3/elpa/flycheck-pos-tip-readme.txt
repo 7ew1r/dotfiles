9b0ad4c4aa8de72540f44f52c3264e83a5222af0
TEWi_R <art.sht.0317@gmail.com>
2017-10-18 15:51:46 +0900

    delete dumpfile

--- .emacs.d/.cask/25.3/elpa/flycheck-pos-tip-readme.txt
+++
@@ -1,6 +0,0 @@
-Provide an error display function to show errors in a tooltip.
-
-Setup
-
-(with-eval-after-load 'flycheck
-  (flycheck-pos-tip-mode))
--- .emacs.d/.cask/25.3/elpa/web-mode-20171016.1216/web-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "web-mode" "20171016.1216" "major mode for editing web templates" '((emacs "23.1")) :commit "e38fb9a95fcf917a1545982e58cbb776b59a95bb" :url "http://web-mode.org" :keywords '("languages"))

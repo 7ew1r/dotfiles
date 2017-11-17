862ffb21aa36c0daae870c61f6fa607794985919
TEWi_R <art.sht.0317@gmail.com>
2017-11-17 10:48:54 +0900

    update

--- .emacs.d/setup_ubuntu.sh
+++ .emacs.d/setup_ubuntu.sh
@@ -5,7 +5,8 @@ sudo apt install -y curl emacs25
 
 #install cask
 curl -fsSL https://raw.githubusercontent.com/cask/cask/master/go | python
-echo "export PATH="$HOME/.cask/bin:$PATH""  >> .bashrc
+echo "export PATH="\$HOME/.cask/bin:\$PATH""  >> ~/.bashrc
+source ~/.bashrc
 cask upgrade
 cd ~/.emacs.d
 cask

0adcd1073a30fd6d110fdc9e32cbd7f38c6aabba
TEWi_R <art.sht.0317@gmail.com>
2017-11-30 01:31:47 +0900

    add Yasnippet

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -354,6 +354,20 @@ mouse-3: delete other windows"
 
 (helm-mode 1)
 
+(use-package yasnippet
+  :ensure t
+  :diminish yas-minor-mode
+  :bind (:map yas-minor-mode-map
+              ("C-x i i" . yas-insert-snippet)
+              ("C-x i n" . yas-new-snippet)
+              ("C-x i v" . yas-visit-snippet-file)
+              ("C-x i l" . yas-describe-tables)
+              ("C-x i g" . yas-reload-all))              
+  :config
+  (yas-global-mode 1)
+  (setq yas-prompt-functions '(yas-ido-prompt))
+  )
+
 ;; diminish
 (defmacro safe-diminish (file mode &optional new-name)
   "https://github.com/larstvei/dot-emacs/blob/master/init.org"

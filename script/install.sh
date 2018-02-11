20d453ab447fe6d3a075f7fdeab151a19d26d773
TEWi_R <art.sht.0317@gmail.com>
2018-02-11 18:30:20 +0900

    delete powerline

--- script/install.sh
+++ script/install.sh
@@ -2,5 +2,5 @@
 ln -sfv $HOME/dotfiles/.zsh $HOME
 ln -sfv $HOME/dotfiles/.tmux.conf $HOME
 ln -sfv $HOME/dotfiles/.emacs.d $HOME
-ln -sfv $HOME/dotfiles/.config/powerline $HOME/.config/powerline
+
 

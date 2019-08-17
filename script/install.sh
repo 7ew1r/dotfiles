948edb9b26557c698362fac1717814a86ca7f890
7ew1r <art.sht.0317@gmail.com>
2019-08-17 17:18:38 +0900

    update install.sh

--- script/install.sh
+++ script/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-ln -sfv $HOME/dotfiles/.zsh $HOME
-ln -sfv $HOME/dotfiles/.tmux.conf $HOME
+#ln -sfv $HOME/dotfiles/.zsh $HOME
+#ln -sfv $HOME/dotfiles/.tmux.conf $HOME
 ln -sfv $HOME/dotfiles/.vimrc $HOME
+ln -sfv $HOME/dotfiles/fish $HOME/.config
+ln -sfv $HOME/dotfiles/fisher $HOME/.config

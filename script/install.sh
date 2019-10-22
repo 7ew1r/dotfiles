223b3de050002d0e18d1afe23c30d8a3cced66c0
7ew1r <art.sht.0317@gmail.com>
2019-10-22 12:05:17 +0900

    feat: add install peco config

--- script/install.sh
+++ script/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 ln -sfv $HOME/dotfiles/.vimrc $HOME
+ln -sfv $HOME/dotfiles/.config/peco $HOME/.config
 ln -sfv $HOME/dotfiles/fish $HOME/.config
 ln -sfv $HOME/dotfiles/fisher $HOME/.config

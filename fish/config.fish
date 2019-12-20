efdbea438c269db6345c5b308e459d0f33844577
7ew1r <art.sht.0317@gmail.com>
2019-12-20 12:03:27 +0900

    fix: pyenv init

--- fish/config.fish
+++ fish/config.fish
@@ -7,14 +7,16 @@ set -g GHQ_SELECTOR peco
 #anyenv
 set PATH $HOME/bin $HOME/.anyenv/bin $PATH
 anyenv init - fish | source
-eval (pyenv init - | source)
+
 
 # thefuck
 thefuck --alias | source 
 
 set PATH /usr/local/bin /usr/sbin $PATH
 
+pyenv init - | source
 # balias
 balias killdss 'find . -name ".DS_Store" | xargs rm'
 balias fishrc 'vim ~/.config/fish/config.fish'
+
 balias update_env 'sh $HOME/src/github.com/7ew1r/shellscripts/update_env.sh'

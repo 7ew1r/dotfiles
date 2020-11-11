d63e2e11fce376558220a9afdb527db3d5eb94a7
7ew1r <art.sht.0317@gmail.com>
2020-11-12 01:29:19 +0900

    add .tmux.conf to install script

--- script/install.sh
+++ script/install.sh
@@ -3,3 +3,4 @@ ln -sfv $HOME/dotfiles/.vimrc $HOME
 ln -sfv $HOME/dotfiles/.config/peco $HOME/.config
 ln -sfv $HOME/dotfiles/fish $HOME/.config
 ln -sfv $HOME/dotfiles/fisher $HOME/.config
+ln -sfv $HOME/dotfiles/.tmux.conf $HOME

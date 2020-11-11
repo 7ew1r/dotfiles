75e7f4d992ffd691912644e00963d776e81c72e7
7ew1r <art.sht.0317@gmail.com>
2020-11-11 21:39:24 +0900

    fix typo

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 function fish_user_key_bindings
-  bind \cr peco_select_history # Bind for prco history to Ctrl+r
+  bind \cr peco_select_history # Bind for peco history to Ctrl+r
 end
 
 set -g GHQ_SELECTOR peco

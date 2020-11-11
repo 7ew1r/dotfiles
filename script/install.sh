ef01b83482a53533008a044d4641461ef2673c12
7ew1r <art.sht.0317@gmail.com>
2020-11-12 02:03:29 +0900

    add neovim setting

--- script/install.sh
+++ script/install.sh
@@ -4,3 +4,4 @@ ln -sfv $HOME/dotfiles/.config/peco $HOME/.config
 ln -sfv $HOME/dotfiles/fish $HOME/.config
 ln -sfv $HOME/dotfiles/fisher $HOME/.config
 ln -sfv $HOME/dotfiles/.tmux.conf $HOME
+ln -sfv $HOME/dotfiles/.config/nvim $HOME/.config
